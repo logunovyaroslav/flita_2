b915b26df140f8452e23055d28c1f9ac4e998294
logunovyaroslav <122658296+logunovyaroslav@users.noreply.github.com>
2023-05-30 21:43:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,49 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-try:
-    # Запрос количества вершин и ребер у пользователя
+# Запрос количества вершин и ребер у пользователя
+number = input("Введите количество вершин: ")
+while not number.isnumeric():
+    print("Невозможное значение")
+    number = input("Введите количество вершин: ")
+n = int(number)
+while n < 0:
+    print("Количество вершин не может быть отрицательным")
     n = int(input("Введите количество вершин: "))
-    while n < 0:
-        print("Количество вершин не может быть отрицательным")
-        n = int(input("Введите количество вершин: "))
 
-    # Создание пустого графа
-    G = nx.Graph()
+# Создание пустого графа
+G = nx.Graph()
 
-    # Добавление вершин в граф
-    for i in range(n):
-        G.add_node(i+1)
-    while True:
-        action = input("Для создания нового соединения напишите - 'add'; для отображения графа напишите - 'draw'; "
-                       "для того что бы закончить напишите - 'end': \n")
-        match action:
-            case 'add':
+# Добавление вершин в граф
+for i in range(n):
+    G.add_node(i+1)
+while True:
+    action = input("Для создания нового соединения напишите - 'add'; для отображения графа напишите - 'draw'; "
+                   "для того что бы закончить напишите - 'end': \n")
+    match action:
+        case 'add':
+            str_x = input("Введите исходящую вершину графа: ")
+            while not str_x.isnumeric():
+                print("Невозможное значение")
+                str_x = input("Введите исходящую вершину графа: ")
+            x = int(str_x)
+            while x > n or x <= 0:
+                print("Невозможное значение")
                 x = int(input("Введите исходящую вершину графа: "))
-                while x > n or x <= 0:
-                    print("Невозможное значение")
-                    x = int(input("Введите исходящую вершину графа: "))
-                y = int(input("Введите приходящую вершину графа: "))
-                while y > n or y <= 0:
-                    print("Невозможное значение")
-                    y = int(input("Введите исходящую вершину графа: "))
-                G.add_edge(x, y)
-            case 'draw':
-                # Отображение графа
-                nx.draw(G, with_labels=True)
-                plt.show()
-            case 'end':
-                break
-            case _:
-                print("Неизвестное действие")
-except ValueError:
-    print("Неправильное значение. Перезапуск программы...")
+            str_y = input("Введите приходящую вершину графа: ")
+            while not str_y.isnumeric():
+                print("Невозможное значение")
+                str_y = input("Введите приходящую вершину графа: ")
+            y = int(str_y)
+            while y > n or y <= 0:
+                print("Невозможное значение")
+                y = int(input("Введите исходящую вершину графа: "))
+            G.add_edge(x, y)
+        case 'draw':
+            # Отображение графа
+            nx.draw(G, with_labels=True)
+            plt.show()
+        case 'end':
+            break
+        case _:
+            print("Неизвестное действие")
